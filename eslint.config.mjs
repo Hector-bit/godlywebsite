6cb9c921e0128030323f2cd9e3e3f017af31ebfd
Hector-bit <hectormartinez.2978@gmail.com>
2025-01-31 05:19:44 -0800

    new eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.mjs
+import { defineConfig } from 'eslint-define-config';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
+export default defineConfig({
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        'args': 'after-used',
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'caughtErrors': 'none',
+      },
+    ],
+  },
 });
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
