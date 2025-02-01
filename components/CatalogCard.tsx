190448ad99cfbcdb755286fcbdd2760c0de9edfa
Hector-bit <hectormartinez.2978@gmail.com>
2025-01-31 19:32:14 -0800

    small ui fixe

--- components/CatalogCard.tsx
+++ components/CatalogCard.tsx
@@ -10,7 +10,7 @@ const CatalogCard = (catalog:CatalogType) => {
           src={`/images/catalog/${catalog.img}`} 
           alt={"song cover"} width={250} height={250}
         />
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col gap-y-2">
           <div className="flex flex-col justify-between">
             <div className="my-2">
               {/* <div className="font-semibold text-textSecondary">January 1st 2001</div> */}
@@ -19,6 +19,7 @@ const CatalogCard = (catalog:CatalogType) => {
               {/* <div className="font-semibold text-textSecondary">Social media: insta, face, youtube, etc.</div> */}
             </div>
           </div>
+          {/* <iframe className="" src="https://open.spotify.com/embed/track/1opv3Bc13TtQE73ECiiE15?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> */}
           <div className="flex flex-row gap-x-2">
             {catalog.spotify && (
               <Link target="_blank" href={catalog.spotify}>
