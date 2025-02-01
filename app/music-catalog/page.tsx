6cbacd1845cc9e30b8ba776ab836e009f2d08973
Hector-bit <hectormartinez.2978@gmail.com>
2025-01-31 19:20:57 -0800

    updated artis info

--- app/music-catalog/page.tsx
+++ app/music-catalog/page.tsx
@@ -7,7 +7,8 @@ const catalogList:CatalogType[] = [
     albumTitle: 'Soul Searching',
     img: 'soulsearching.jpg',
     artist: 'Chelsey Monique',
-    spotify: 'https://open.spotify.com/album/2traKNQMVyH8Pl9s1wInaA?si=BGyzv78cSf6KubvHVCnchw'
+    spotify: 'https://open.spotify.com/album/2traKNQMVyH8Pl9s1wInaA?si=BGyzv78cSf6KubvHVCnchw',
+    youtube: 'https://www.youtube.com/watch?v=4s84mFA8edI'
   },
   {
     albumTitle: 'Stavo',
@@ -17,6 +18,24 @@ const catalogList:CatalogType[] = [
     spotify: 'https://open.spotify.com/album/7bthcmPk5NDr3N6wMjocrk?si=TRdzy07nSBqEzRUKr_zwXg',
     youtube: 'https://www.youtube.com/playlist?list=OLAK5uy_kQcJPro6ZT1kA_4gNcN_H31kc87QTItAQ',
     appleMusic: 'https://music.apple.com/us/album/stavo/1733600211'
+  },
+  {
+    albumTitle: 'Dsmirk',
+    img: 'want2.jpg',
+    artist: 'Dsmirk',
+    instagram: 'https://www.instagram.com/whodat.d_smirkyuh/',
+    spotify: 'https://open.spotify.com/album/7bthcmPk5NDr3N6wMjocrk?si=TRdzy07nSBqEzRUKr_zwXg',
+    youtube: 'https://www.youtube.com/watch?v=3so0KhMlQHk',
+    appleMusic: 'https://music.apple.com/us/album/want-2-feat-meech-da-don/1706853780?i=1706853782'
+  },
+  {
+    albumTitle: 'XEON',
+    img: 'xeon.jpg',
+    artist: 'XEON',
+    instagram: 'https://www.instagram.com/ltmxeon/',
+    spotify: 'https://open.spotify.com/track/1opv3Bc13TtQE73ECiiE15?si=79be8324811246c6',
+    youtube: 'https://youtu.be/VcWg9yNenpE?si=Zy43Nle2ub_urAGB',
+    appleMusic: 'https://music.apple.com/us/album/4-deep/1727769711?i=1727769722'
   },
 ]
 
