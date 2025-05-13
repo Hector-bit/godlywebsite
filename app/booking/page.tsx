e9d9ab7867886b6c88d010d7aa1f9f55bfb50607
Hector-bit <hectormartinez.2978@gmail.com>
2025-05-12 22:21:14 -0700

    google form

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Booking = () => {
   return (
     <div className="flex flex-col gap-2 font-bold">
+      <div>
+        <span>Client Form: </span>
+        <Link className="text-cyan-500" href={"https://forms.gle/HLqi91taBXNpBWvz5"}>Google Form</Link>
+      </div>
       <div>Send us an email at godlyproductionsmt@gmail.com</div>
       <div>Call at <a href="tel:+15645651717">+115645651717</a></div>
       <Image src={"/images/godlyMenu.png"} alt={"godly productions menu"} width={600} height={1000}/>
--- components/EventsList.tsx
+++ components/EventsList.tsx
@@ -13,8 +13,8 @@ const EventList = (props: {eventsList: EventType[]}) => {
         const eventLink = event.link ? event.link : 'https://godlyproductions.com/'
 
         return (
-          <div key={event._id} className="flex flex-col sm:flex-row justify-between border-y-2 border-white p-4 rounded-xl gap-6">
-            <div className="flex flex-col gap-3 w-1/3">
+          <div key={event._id} className="flex flex-col sm:flex-row justify-between border-y-2 border-white py-8 px-4 rounded-xl gap-6">
+            <div className="flex flex-col gap-3 sm:w-1/3">
               <div className="font-bold text-xl">{event.title}</div>
               <Image className="max-h-auto w-full border border-white rounded-xl" src={eventImg} alt={"event"} width={600} height={600}/>
             </div>
